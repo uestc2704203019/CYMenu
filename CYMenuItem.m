7a52beb5d4fcc8dd9971f8945c4867a43f31ad3a
柴源 <769099058@qq.com>
2014-01-02 14:20:50 +0800

    Create CYMenuItem.m

---
+++ CYMenuItem.m
@@ -0,0 +1,32 @@
+//
+//  CYMenuItem.m
+//  loveLife
+//
+//  Created by chai yuan on 13-12-31.
+//  Copyright (c) 2013年 chai yuan. All rights reserved.
+//
+
+#import "CYMenuItem.h"
+
+@implementation CYMenuItem
+@synthesize sort;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
