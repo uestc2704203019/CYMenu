447b2347ec928d46dffc0c296b4d8fd226618e7d
柴源 <769099058@qq.com>
2014-01-02 14:20:32 +0800

    Create CYMenuItem.h

---
+++ CYMenuItem.h
@@ -0,0 +1,15 @@
+//
+//  CYMenuItem.h
+//  loveLife
+//
+//  Created by chai yuan on 13-12-31.
+//  Copyright (c) 2013年 chai yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYMenuItem : UIView
+
+@property (nonatomic, assign) NSInteger sort;
+
+@end
